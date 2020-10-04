d594b5422231620bb3985264083539ea88e1edf9
geocode-matt <mlindenx@gmail.com>
2020-10-04 13:33:54 -0500

    Updates

---
+++ precache-manifest.6ac3f787e22fad41ea65ba5e8095990d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55afc13a163bc784d54f80e553cde0e2",
+    "url": "/matt-linden/index.html"
+  },
+  {
+    "revision": "c0e529fa45b4ea0f9d76",
+    "url": "/matt-linden/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "6397a72831f349602b82",
+    "url": "/matt-linden/static/css/main.20ba8122.chunk.css"
+  },
+  {
+    "revision": "c0e529fa45b4ea0f9d76",
+    "url": "/matt-linden/static/js/2.976953c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/matt-linden/static/js/2.976953c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6397a72831f349602b82",
+    "url": "/matt-linden/static/js/main.6fe078dc.chunk.js"
+  },
+  {
+    "revision": "662ebab94178a72bc346",
+    "url": "/matt-linden/static/js/runtime-main.f79c72a4.js"
+  },
+  {
+    "revision": "4afb08ad35fef181d7bd2bad3eca5b31",
+    "url": "/matt-linden/static/media/barkeep.4afb08ad.jpg"
+  },
+  {
+    "revision": "e78e523caf8ef29182f2667519ede8a1",
+    "url": "/matt-linden/static/media/bg7.e78e523c.jpg"
+  },
+  {
+    "revision": "0b5172ae640a9bb118b71674a8519051",
+    "url": "/matt-linden/static/media/gameplan-realty-team.0b5172ae.jpg"
+  },
+  {
+    "revision": "10683c9c396af6e858f7e2211ee57110",
+    "url": "/matt-linden/static/media/password-generatorv2.10683c9c.jpg"
+  },
+  {
+    "revision": "40a8991eae0bdc3aa2c9f3070d696deb",
+    "url": "/matt-linden/static/media/password-generatorv2.40a8991e.png"
+  },
+  {
+    "revision": "61f81ae5ca6b5ed63a16f23f278314b8",
+    "url": "/matt-linden/static/media/simple-social.61f81ae5.jpg"
+  },
+  {
+    "revision": "a4140d3e49e63bc3684db4f3bf04c2f6",
+    "url": "/matt-linden/static/media/team-profile-generator.a4140d3e.jpg"
+  },
+  {
+    "revision": "367a3be435996d544c0481dfd384ec83",
+    "url": "/matt-linden/static/media/tech-blog.367a3be4.jpg"
+  },
+  {
+    "revision": "17ea0963cd81725906b42a9b27a86356",
+    "url": "/matt-linden/static/media/weather-dashboardv2.17ea0963.png"
+  },
+  {
+    "revision": "5ec74154929256bcd5e80dfeb69fad90",
+    "url": "/matt-linden/static/media/weather-dashboardv2.5ec74154.jpg"
+  },
+  {
+    "revision": "194cfa302deae0d09b0271587ec23ca8",
+    "url": "/matt-linden/static/media/work-day-scheduler.194cfa30.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.8fc0afd68099fbda0a27024d97d85601.js"
+  "/matt-linden/precache-manifest.6ac3f787e22fad41ea65ba5e8095990d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/matt-linden/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
